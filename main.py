dd6b5ec7565d7e743f2052b8c98079435d2b9a04
Danny <daniil4math@gmail.com>
2024-06-26 18:25:15 +0100

    Added inventory

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from map_objects import Map
 from player import Player
 import constants
 from progress_bar import map, map_generator, percentage_calc, text, textRect, font
+from inventory import InventoryItem, PlayerInventory
 
 pygame.init()
 window = pygame.display.set_mode((constants.WINDOW_HEIGHT, constants.WINDOW_WIDTH))
@@ -17,17 +18,28 @@ game_surface = pygame.Surface(map.map_img.get_size())
 delay = 0
 minimap = None
 
+robe_hood = InventoryItem("assets\png\walkcycle\HEAD_robe_hood.png")
+inventory = PlayerInventory()
+inventory.common_slots[2][2] = robe_hood
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if inventory.shown:
+            inventory.handle_events(event)
+        if event.type == pygame.MOUSEBUTTONDOWN and map.is_loaded and not inventory.shown:
             if event.button == pygame.BUTTON_LEFT:
                 map_col = round((-map.offset_px.x + event.pos[0])//32 + map.offset_tiles.x)
                 map_row = round((-map.offset_px.y + event.pos[1])//32 + map.offset_tiles.y)
                 player.set_target(pygame.Vector2(map_col, map_row), map.map)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                inventory.shown = not inventory.shown
 
     pressed = pygame.key.get_pressed()
+    if not inventory.shown:
+        map.handle_pressed(pressed)
     map.handle_pressed(pressed)
     window.fill((200, 0, 0))
     
@@ -67,7 +79,8 @@ while True:
         if minimap == None:
             minimap = map.get_minimap(map.map)
         window.blit(game_surface, map.offset_px)     
-        window.blit(minimap, (0, 0))   
+        window.blit(minimap, (0, 0))  
+        inventory.display(window) 
         delay += 1
             
     pygame.display.update()
